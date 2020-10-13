f1abf6f55cfec3a1399682fb33fb8a3abfefba3f
Rikardo <rikardo@marenzzi.de>
2020-10-13 15:27:20 +0200

    first setps

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="App">
+            <img src="src/images/dom.jpg" alt=""/>
+            <h1>Leeve Jroß vum Rhing</h1>
+        </div>
+    );
+}
+
+export default Header;
